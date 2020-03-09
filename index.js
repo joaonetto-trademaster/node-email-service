1db6a0a3851d6bf9cb5e3ea39ab8b63022e29d14
João Netto <joaonettopb@hotmail.com>
2020-03-09 16:53:53 -0300

    feat(project): created function for email service

---
+++ index.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: '.env' });
+}
+
+const bodyParser = require('body-parser');
+const app = require('express')();
+const sendEmail = require('./sender');
+const port = process.env.PORT;
+
+// parse application/x-www-form-urlencoded and application/json
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.listen(port, () => console.log(`Email Service API listening on port ${port}!`));
+
+app.post('/email', (req, res) => {
+  const {
+    templateName,
+    from,
+    to,
+    subject,
+    name,
+    confirm_account_url
+  } = req.body;
+
+  const data = {
+    templateName,
+    from,
+    to,
+    subject,
+    name,
+    confirm_account_url
+  };
+
+  //pass the data object to send the email
+  sendEmail(data);
+});
---
+++ sender.js
@@ -0,0 +1,31 @@
+const sgMail = require("@sendgrid/mail");
+
+module.exports = sendEmail = (data) => {
+  sgMail.setApiKey(process.env.SENDGRID_API);
+
+  templates = {
+    password_reset_confirm: "4980d32f-8a18-453f-bfab-527d49e81e3a",
+    password_reset: "8f919725-bcbf-46f1-aac8-2ac2d37e3400",
+    confirm_account: "d-cb3ca7e305be4329b8fff35bb9de3d60",
+  };
+
+  const msg = {
+    to: data.to,
+    from: data.from,
+    templateId: templates[data.templateName],
+    subject: data.subject,
+    dynamic_template_data: {
+      name: data.name,
+      confirm_account_url: data.confirm_account_url,
+      reset_password_url: data.reset_password_url
+    }
+  };
+
+  sgMail.send(msg, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent successfully');
+    }
+  });
+}
